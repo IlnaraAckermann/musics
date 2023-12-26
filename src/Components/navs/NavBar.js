626077446ceeb599f2f90475d85a088404007b9c
Ilnara Ackermann <ilnara.ackermann@hotmail.com>
2023-12-26 09:37:20 -0300

    feat: criado o component NavBar

---
+++ src/Components/navs/NavBar.js
@@ -0,0 +1,36 @@
+import "./style.css";
+function NavBar() {
+	return (
+		<>
+			<div className="header-container">
+				<a href="/">
+					<h1 className="logo">My Musics</h1>
+				</a>
+				<nav>
+					<input
+						type="checkbox"
+						id="menu-hamburguer"
+					/>
+					<label htmlFor="menu-hamburguer">
+						<div className="menu">
+							<span className="hamburguer" />
+						</div>
+					</label>
+					<ul>
+						<li>
+							<a href="/">About</a>
+						</li>
+						<li>
+							<a href="/albuns">Albuns</a>
+						</li>
+						<li>
+							<a href="#contact">Contact</a>
+						</li>
+					</ul>
+				</nav>
+			</div>
+		</>
+	);
+}
+
+export default NavBar;
