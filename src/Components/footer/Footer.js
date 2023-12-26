bbaa52d31c0b755a4af83cadbf4c2caca5a12dde
Ilnara Ackermann <ilnara.ackermann@hotmail.com>
2023-12-26 10:03:31 -0300

    feat(footer): Adicionado componente Footer

---
+++ src/Components/footer/Footer.js
@@ -0,0 +1,25 @@
+import "./style.css";
+
+function Footer() {
+    return ( <>
+     <footer>
+  
+      <small>
+        Site desenvolvido por <strong>@IlnaraAckermann</strong>
+        <br />
+
+        <a href="https://github.com/IlnaraAckermann" target="_blank"  title="GitHub" rel="noreferrer">        
+          <i className="fab fa-github">GitHub</i>        
+        </a>
+
+        <a href="https://www.linkedin.com/in/ilnaraackermann" target="_blank" title="LinkedIn" rel="noreferrer">
+          <i className="fab fa-linkedin">LinkedIn</i>      
+        </a>
+      </small>
+
+  </footer>
+
+    </> );
+}
+
+export default Footer;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import NavBar from './Components/navs/NavBar';
+import Footer from './Components/footer/Footer';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     < NavBar />
+    <Footer />
   </React.StrictMode>
 );
 
