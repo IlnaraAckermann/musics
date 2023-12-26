88a28d8601aac225e41ade91fa79e208b4d386c6
Ilnara Ackermann <ilnara.ackermann@hotmail.com>
2023-12-26 20:54:12 -0300

    feat(modal): Adiciona o componente modal

---
+++ src/Components/modal/Modal.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./style.css";
+
+export default function Modal({ isOpen, children, setModalOpen }) {
+	if (isOpen) {
+		return (
+			<div className="modal">
+				<div className="modal-content">
+					<button onClick={setModalOpen}>
+						<i class="fa-solid fa-rectangle-xmark"></i>
+					</button>
+					<div>{children}</div>
+				</div>
+			</div>
+		);
+	} else {
+		return null;
+	}
+}
