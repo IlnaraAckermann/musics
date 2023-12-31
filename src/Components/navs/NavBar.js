7037b7ea1c0c2563a2156f49641707de9ea3cd30
Ilnara Ackermann <ilnara.ackermann@hotmail.com>
2023-12-31 12:40:11 -0300

    refactor(layout):  Extraida lógica visual para o componente CustomLink na NavBar
    Refatorei o componente NavBar para melhorar o gerenciamento do layout, extraindo a lógica visual para o componente CustomLink. Adicionei o CustomLink para melhorar o gerenciamento da navegação e a representação visual dos links ativos.

--- src/Components/navs/NavBar.js
+++ src/Components/navs/NavBar.js
@@ -1,11 +1,51 @@
+import React, { useEffect, useState } from "react";
+import ApiService from "../../services/ApiService";
 import "./style.css";
-function NavBar() {
+import { Link, useMatch, useResolvedPath } from "react-router-dom";
+
+export default function NavBar() {
+	const [loading, setLoading] = useState(false);
+	const [dataIsEmpty, setDataIsEmpty] = useState(true);
+	const [albuns, setAlbuns] = useState([]);
+
+	useEffect(() => {
+		const fetchAlbuns = async () => {
+			try {
+				const albunsData = await ApiService.getAlbumIds();
+				setAlbuns(albunsData);
+				if (albunsData.length !== 0) {
+					setDataIsEmpty(false);
+				} else {
+					setDataIsEmpty(true);
+				}
+			} catch (e) {
+				console.error("Erro ao obter os albuns");
+			}
+		};
+		fetchAlbuns();
+	}, [albuns]);
+
+	const handleClick = async () => {
+		if (loading) {
+			return;
+		}
+		setLoading(true);
+		const url = "https://jsonplaceholder.typicode.com/photos";
+		try {
+			await ApiService.postUrl(url);
+			window.alert("Dados carregados com sucesso");
+			setDataIsEmpty(false);
+		} catch (error) {
+			console.error("Erro na requisição:", error.message);
+		}
+	};
+
 	return (
 		<>
 			<div className="header-container">
-				<a href="/">
+				<Link to="/">
 					<h1 className="logo">My Musics</h1>
-				</a>
+				</Link>
 				<nav>
 					<input
 						type="checkbox"
@@ -17,14 +57,10 @@ function NavBar() {
 						</div>
 					</label>
 					<ul>
-						<li>
-							<a href="/">Home</a>
-						</li>
-						<li>
-							<a href="/albuns">Albuns</a>
-						</li>
-						<li>
-							<a href="/photos">Sincronizar Dados</a>
+						<CustonLink to='/'>Home</CustonLink>
+						<CustonLink to='/albuns'>Albuns</CustonLink>
+						<li className={dataIsEmpty ? "" : "d-none"}>
+							<button onClick={handleClick}>Sincronizar Dados</button>
 						</li>
 					</ul>
 				</nav>
@@ -33,4 +69,16 @@ function NavBar() {
 	);
 }
 
-export default NavBar;
+function CustonLink({ to, children, ...props }) {
+	const resolvedPath = useResolvedPath(to)
+	const isActive =useMatch({path: resolvedPath.pathname, end:true})
+	return (
+		<li className={isActive ? 'active':''}>
+			<Link to={to} {...props}>
+				{children}
+			</Link>
+		</li>
+	);
+}
+
+
