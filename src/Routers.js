3c448e801c250c86fd9badbfb1f15ffa746c1bf5
Ilnara Ackermann <ilnara.ackermann@hotmail.com>
2024-01-01 00:23:06 -0300

    Update: Atualizada a logica para o correto funcionamento das rotas.

--- src/Routers.js
+++ src/Routers.js
@@ -1,19 +1,18 @@
-import React from 'react'
 import AlbumId from './pages/AlbumId';
 import AlbumPhotoList from './pages/AlbumPhotoList';
 import Home from './pages/Home';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route, useNavigate } from 'react-router-dom';
 
 
 const AppRouters = () => {
+ 
+  const navigate = useNavigate();
     return ( 
-        <Router>
             <Routes>
                 <Route path='/' element={<Home/>}></Route>
                 <Route path='/albuns' element={<AlbumId/>}></Route>
-                <Route path='/album/:id' element={<AlbumPhotoList/>}></Route>
+                <Route path='/album/:id' element={<AlbumPhotoList  navigate={navigate}/>}></Route>
             </Routes>
-        </Router>
      );
 }
 
