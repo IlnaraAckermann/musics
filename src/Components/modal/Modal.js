220b2efc6cb1dd8d24f97bb485f1e62b32a90310
Ilnara Ackermann <ilnara.ackermann@hotmail.com>
2024-01-03 01:24:20 -0300

    chore: Adição do role="dialog"

--- src/Components/modal/Modal.js
+++ src/Components/modal/Modal.js
@@ -4,7 +4,7 @@ import "./style.css";
 export default function Modal({ isOpen, children, setModalOpen }) {
 	if (isOpen) {
 		return (
-			<div className="modal">
+			<div className="modal" role="dialog">
 				<div className="modal-content">
 					<button onClick={setModalOpen}>
 						<i className="fa-solid fa-rectangle-xmark"></i>
